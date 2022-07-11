31778ed2269af42517a7c35ba3eba89a0c5ce3e8
anthem-landing18 <imokenpi2011@gmail.com>
2022-07-12 00:13:32 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+class Square extends React.Component {
+    // コンストラクタ
+    constructor(props) {
+        // 初期化
+        super(props);
+        // stateを初期化する
+        this.state = {
+            // 初期状態用にnullをセットする
+            squares: Array(9).fill(null),
+        };
+    }
+
+    render() {
+        return (
+            // 渡された値を表示する
+            // クリックされた場合はonClickを発火する
+            <button className="square"
+                onClick={() => this.props.onClick()}
+            >
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        // 正方形のコンポーネントを呼び出す
+        return (
+            <Square
+                value={this.state.squares[i]}
+                // ここでsquareのonclickを発火する
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);

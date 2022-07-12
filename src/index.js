e3b57b2bf169d829853fab14397ebf7db3ae158a
anthem-landing18 <imokenpi2011@gmail.com>
2022-07-13 02:06:09 +0900

    ゲーム状態の管理をGameに移譲

--- src/index.js
+++ src/index.js
@@ -62,20 +62,8 @@ class Board extends React.Component {
     }
 
     render() {
-        // 勝敗の判定
-        const winner = calculateWinner(this.state.squares);
-
-        // 勝負はついている場合はメッセージを出す
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -97,14 +85,43 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    // 履歴は親のGameコンポーネントで管理する
+    constructor(props) {
+        super(props);
+        this.state = {
+            // 履歴を管理する
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        };
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+
+        // 勝敗の判定
+        const winner = calculateWinner(current.squares);
+
+        // 勝負はついている場合はメッセージを出す
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
+                    <div>{status}</div>
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
